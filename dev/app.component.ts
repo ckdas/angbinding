1f35828489feefd410b3f1fa78334330a18c4ebe
Chiranjit <ckdas.bcs@gmail.com>
2016-09-22 09:37:15 +0530

    Data Binding string, properties, directive, event, two-way

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -6,21 +6,37 @@ import {Component} from 'angular2/core';
     template: `
 			{{'Hey'}}, 
 			{{name}},
-			{{onTest()}}
+			{{onTest()}}<BR>
 			<input type="text" 
-				class="{{'rade'}}" 
+				class="{{'rade'}}"
 				value="{{name}}" 
 				#inpname
 				(keyup)="0"
 			><BR>
 			{{inpname.value}}
+			<BR>
+			<input 
+				type="text"
+				(keyup)="onKeydown (inputElement.value)"
+				#inputElement
+			>
+			<BR><p>{{values}}</p>
+			<BR><BR>Two Way Binding<BR>
+			<input type="text" [(ngModel)]="name">
+			<p>Your Name: {{name}}</p>
     `,
 })
 export class AppComponent {
-	name = "Chiranjit"
+	name = "Chiranjit";
+	values = "";
 
 	onTest() {
 		return 1 === 1;
+	}
+
+	onKeydown(value: string) {
+		this.values += value + ' | ';
+	
 	}
 
 }
